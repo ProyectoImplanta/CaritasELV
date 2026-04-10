7106859d4e6baa47b30f52fc040d3340fbad3afc
darkfrozen132 <darkfrozen@outlook.com>
2026-04-10 01:11:26 -0500

    asd

--- prohibir_mains.sh
+++ prohibir_mains.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-# Obtener el nombre de la rama actual
 branch="$(git rev-parse --abbrev-ref HEAD)"
 
-# Definir las ramas donde está prohibido hacer commit
+# 1. Bloqueo de Commits directos
 if [ "$branch" = "main" ] || [ "$branch" = "develop" ]; then
-  echo "----------------------------------------------------------"
-  echo "❌ ERROR: No puedes hacer commit directamente en '$branch'."
-  echo "Debes trabajar en una rama de tarea y subir a 'integration'."
-  echo "----------------------------------------------------------"
+  # Revisar si es un merge
+  if [ -f .git/MERGE_HEAD ]; then
+    echo "--------------------------------------------------------"
+    echo "❌ BLOQUEO DE MERGE: No puedes mergear nada aquí localmente."
+    echo "Haz el Merge a través de un Pull Request en GitHub web."
+    echo "--------------------------------------------------------"
+    # Cancelar el merge
+    git merge --abort
+    exit 1
+  fi
+
+  echo "--------------------------------------------------------"
+  echo "❌ BLOQUEO DE COMMIT: No puedes crear commits en $branch."
+  echo "--------------------------------------------------------"
   exit 1
 fi
